fd5ed2dfd61818127a10847c321673ec47179a6c
nishal <nishalk9399@gmail.com>
2023-09-30 21:10:15 +0530

    pushing updated script

--- user.sh
+++ user.sh
@@ -78,7 +78,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # give full path of user.service because we are inside /app
-cp /root/roboshop-shell/user.service /etc/systemd/system/user.service &>>$LOGFILE
+cp /root/roboshop-shell-tf/user.service /etc/systemd/system/user.service &>>$LOGFILE
 
 VALIDATE $? "copying user.service"
 
@@ -94,7 +94,7 @@ systemctl start user &>>$LOGFILE
 
 VALIDATE $? "Starting user"
 
-cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp /root/roboshop-shell-tf/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "Copying mongo repo"
 
